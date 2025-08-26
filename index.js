068ef4d9eda1965835b8f330901883d885eb25e6
rajdas9707 <97969994+rajdas9707@users.noreply.github.com>
2025-08-26 01:18:55 +0000

    initiakl commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// server.js
+const express = require("express");
+var cors = require('cors')
+
+
+
+const app = express();
+const PORT = 1234;
+
+
+app.use(cors())
+// Middleware to parse JSON body
+app.use(express.json());
+
+// GET request example
+app.get("/", (req, res) => {
+  res.send("Hello! This is a GET request.");
+});
+
+// POST request example
+app.post("/data", (req, res) => {
+  const receivedData = req.body;
+  res.json({
+    message: "POST request received!",
+    yourData: receivedData,
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
+});
